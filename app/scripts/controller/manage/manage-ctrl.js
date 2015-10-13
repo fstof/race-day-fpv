e20b888b770a6f8c089a05a5d98e59c5d8cacc5a
Frans Stofberg <frans.stofberg@gmail.com>
2015-10-14 00:19:00 +0200

    more management

--- app/scripts/controller/manage/manage-ctrl.js
+++ app/scripts/controller/manage/manage-ctrl.js
@@ -3,26 +3,37 @@
 angular.module('race-day-fpv')
 	.controller('ManageCtrl', ManageCtrl);
 
-function ManageCtrl(FPVSession, RaceGroup, ngToast) {
+function ManageCtrl(FPVSession, RaceGroup, Frequency, ngToast) {
 	var self = this;
 	self.signedIn = FPVSession.user !== null;
 	self.groups = {};
+	self.frequencies = {};
 	_init();
 
 	function _init() {
 		//self.groups = RaceGroup.all;
-		var all = RaceGroup.all();
-		all.on('child_added', function (snap) {
+		var allGrp = RaceGroup.all();
+		allGrp.on('child_added', function (snap) {
 			self.groups[snap.key()] = snap.val();
 			self.groups[snap.key()].key = snap.key();
 
 		});
-		all.on('child_removed', function (snap) {
+		allGrp.on('child_removed', function (snap) {
 			delete self.groups[snap.key()];
+		});
+
+		var allFreq = Frequency.all();
+		allFreq.on('child_added', function (snap) {
+			self.frequencies[snap.key()] = snap.val();
+			self.frequencies[snap.key()].key = snap.key();
+
+		});
+		allFreq.on('child_removed', function (snap) {
+			delete self.frequencies[snap.key()];
 		});
 	}
 
-	self.update = function (group) {
+	self.updateGroup = function (group) {
 		RaceGroup.save(group.key, group, function (err) {
 			if (err) {
 				ngToast.danger('Group not saved');
@@ -52,5 +63,39 @@ function ManageCtrl(FPVSession, RaceGroup, ngToast) {
 			}
 		});
 	};
+
+	self.updateFreq = function (freq) {
+		Frequency.save(freq.key, freq, function (err) {
+			if (err) {
+				ngToast.danger('Frequency not saved');
+			} else {
+				ngToast.success('Frequency saved');
+			}
+		});
+	};
+
+	self.addFreq = function () {
+		var obj = {
+			description: 'Fat1, F1',
+			frequency: '5740'
+		};
+		Frequency.create(obj, function (err) {
+			if (err) {
+				ngToast.danger('Frequency not added');
+			} else {
+				ngToast.success('Frequency added');
+			}
+		});
+	};
+
+	self.deleteFreq = function (freq) {
+		Frequency.delete(freq.key, function (err) {
+			if (err) {
+				ngToast.danger('Frequency not deleted');
+			} else {
+				ngToast.success('Frequency deleted');
+			}
+		});
+	};
 }
-ManageCtrl.$inject = ['FPVSession', 'RaceGroup', 'ngToast'];
+ManageCtrl.$inject = ['FPVSession', 'RaceGroup', 'Frequency', 'ngToast'];
---
+++ app/scripts/service/frequency.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('race-day-fpv')
+	.factory('Frequency', Frequency);
+
+function Frequency(FIREBASE_REF, $firebaseArray) {
+	var ref = FIREBASE_REF;
+
+	var frequencies = $firebaseArray(ref.child('frequencies').orderByChild('frequency'));
+
+	return {
+		//all: raceGroups,
+		all: function () {
+			return ref.child('frequencies').orderByChild('frequency');
+		},
+		get: function (id) {
+			return ref.child('frequencies').child(id);
+		},
+		create: function (frequency, callback) {
+			return ref.child('frequencies').push(frequency, callback);
+		},
+		delete: function (id, callback) {
+			return ref.child('frequencies/' + id).set(null, callback);
+		},
+		save: function (id, frequency, callback) {
+			return ref.child('frequencies/' + id).set(frequency, callback);
+		}
+	};
+}
+
+Frequency.$inject = ['FIREBASE_REF', '$firebaseArray'];
